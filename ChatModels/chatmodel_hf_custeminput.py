6b073c99788a7d6bfb5fdd15a4de2c2e59061f0f
Harshvardhan <harshyop829196@gmail.com>
2026-05-12 22:45:41 +0530

    updated custem msg ,chathistry with lableas

---
+++ ChatModels/chatmodel_hf_custeminput.py
@@ -0,0 +1,48 @@
+from langchain_core.messages import  HumanMessage, SystemMessage, AIMessage
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+
+from dotenv import load_dotenv
+
+from langchain_core.messages import  HumanMessage, SystemMessage, AIMessage
+
+import os #for accessing env variable
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    huggingfacehub_api_token=os.getenv("HF_TOKEN"),
+    task="text2text-generation",
+    temperature=0.5
+)
+
+model = ChatHuggingFace(llm=llm)
+chat_history = [] ## this is the list which will store the conversation history between user and model
+while True:
+    query = input("Hello how i can assist you: ")
+    chat_history.append( HumanMessage(content =query)) ## we are appending the user query to the chat history list with the label "user"  
+    if query.lower() == 'exit':
+        print(chat_history) ## this will print the conversation history between user and model when user exit the chat
+        break
+    
+    result = model.invoke(chat_history) ## we are passing the chat history to the model so that it can connect the two query together and give us the correct ans which is 90.
+    
+    chat_history.append(AIMessage(content=result.content)) ## we are appending the model response to the chat history list with the label "model"
+    
+    print(f"AI: {result.content}")
+    
+
+# Hello how i can assist you: tell me which is greater 5, 9?
+# AI: 9 is greater than 5.
+# Hello how i can assist you: now multipy this number with 10 
+# AI: However, I don't see a number provided. Please provide the number you'd like to multiply by 10.
+# Hello how i can assist you: _______- this means it dont remenber the previous conversation and it is not able to connect the two query together.  
+# there for we need to give the conversation history to the model so that it can connect the two query together and give us the correct ans which is 90.
+
+# Hello how i can assist you: which is greater 5 or 6
+# AI: 6 is greater than 5.
+# Hello how i can assist you: now multiply it with 10 
+# AI: 6 multiplied by 10 is 60.
+# Hello how i can assist you: 
+
+# there is a problem we dont know that which is user qurey and which is model response so we can give label to the user query and model response in the chat history list so that it will be more clear and we can easily understand the conversation history between user and model. 
+# so we can append the user query to the chat history list with the label "user" and model response with the label "model" so that it will be more clear and we can easily understand the conversation history between user and model.    
---
+++ ChatModels/messages.py
@@ -0,0 +1,26 @@
+from langchain_core.messages import  HumanMessage, SystemMessage, AIMessage
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+#human message is the message which we give to the model and system message is the message which we give to the model to set the context of the conversation and AI message is the message which we get from the model as a response.
+#ai message is the message which we get from the model as a response. it also contains metadata along with the response. we can access the response by using result.content and metadata by using result.metadata
+#system message is the message which we give to the model to set the context of the conversation. it is optional but it is good to set the context of the conversation to get better response from the model.
+
+
+import os   
+load_dotenv()   ##loads env variable    
+token = os.getenv("HF_TOKEN")   
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    huggingfacehub_api_token=token,
+    task="text2text-generation",
+    temperature=0.5## temperature is the creativity of the model , higher the temperature more creative ans will be. 
+    )   
+message = [
+    SystemMessage(content="You are a helpful assistant."), ## this is the system message which we give to the model to set the context of the conversation. it is optional but it is good to set the context of the conversation to get better response from the model.
+    HumanMessage(content="tell me about langchain?") ## this is the human message which we give to the model as a query.
+]
+model = ChatHuggingFace(llm=llm) ## we are creating an instance of the ChatHuggingFace class and passing the llm object to it.
+result = model.invoke(message) ## we are invoking the model with the message which we have created and getting the response from the model.
+message.append(AIMessage(content=result.content)) ## we are appending the model response to the message list with the label "AIMessage" so that it will be more clear and we can easily understand the conversation history between user and model. 
+print(message) ## this will print the response from the model
